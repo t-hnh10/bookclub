482e9becab1a185ad392cf096d12f190a823d4a1
Tony Huynh <t.hnh@tuta.io>
2019-06-18 11:57:26 +1000

    created & dropped member and address models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_013706) do
+ActiveRecord::Schema.define(version: 2019_06_18_015456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
