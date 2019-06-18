a3aa43ddcb5d38d880acd7c1f958eb14978782e9
Tony Huynh <t.hnh@tuta.io>
2019-06-18 11:39:16 +1000

    added Book and Author model & seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.destroy_all
+
+heinlein = Author.create(first_name: "Robert", last_name: "Heinlein")
+gaiman = Author.create(first_name: "Neil", last_name: "Gaiman")
+kastrup = Author.create(first_name: "Bernardo", last_name: "Kastrup")
+nin = Author.create(first_name: "Anais", last_name: "Nin")
+
+heinlein.books.create(title: "Stranger in a Strange Land")
+heinlein.books.create(title: "Friday")
+heinlein.books.create(title: "The Moon is a Harsh Mistress")
+
+gaiman.books.create(title: "American Gods")
+gaiman.books.create(title: "Neverworld")
+gaiman.books.create(title: "Anansi Boys")
+
+kastrup.books.create(title: "Brief Peek Beyond")
+
+nin.books.create(title: "Little Birds")
+nin.books.create(title: "Delta of Venus")heinlein.books.create(title: "Stranger in a Strange Land")
