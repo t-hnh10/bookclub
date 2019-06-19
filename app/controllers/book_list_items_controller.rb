ad25f8ce72aa55e439f6f880538ac3575270fa2d
Tony Huynh <t.hnh@tuta.io>
2019-06-19 12:05:38 +1000

    completed my list implementation

--- app/controllers/book_list_items_controller.rb
+++ app/controllers/book_list_items_controller.rb
@@ -1,2 +1,5 @@
 class BookListItemsController < ApplicationController
+    def index
+        @books = BookListItem.books_for_current_user(current_user)
+    end
 end
--- app/models/book_list_item.rb
+++ app/models/book_list_item.rb
@@ -1,4 +1,8 @@
 class BookListItem < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  def self.books_for_current_user(current_user)
+    return BookListItem.where(user_id: current_user.id).order(:updated_at)
+  end
 end
