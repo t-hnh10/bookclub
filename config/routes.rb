21ba56f3cde155da06928c4a51b265b96d251094
Tony Huynh <t.hnh@tuta.io>
2019-06-25 10:53:00 +1000

    added add book form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "books#index"
   get "/books", to: "books#index", as: "books"
-  get "books/add", to: "books#add", as: "add_book_to_list"
+  get "/books/new", to: "books#new", as: "new_book"
+  post "books", to: "books#create"
+  get "/books/add", to: "books#add", as: "add_book_to_list"
   get "/books/:id", to: "books#show", as: "book"
   get "/authors", to: "authors#index", as: "authors"
   get "/authors/:id", to: "authors#show", as: "author"
