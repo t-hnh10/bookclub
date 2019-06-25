9d7f4fef435b18d398509f97364c81017f0f557c
Tony Huynh <t.hnh@tuta.io>
2019-06-25 11:34:09 +1000

    alternative destroy method

--- app/controllers/book_list_items_controller.rb
+++ app/controllers/book_list_items_controller.rb
@@ -9,4 +9,10 @@ class BookListItemsController < ApplicationController
         BookListItem.destroy(item_id)
         redirect_to book_list_items_path
     end
+
+    # def destroy
+    #     # .where returns an array (even with only one result) hence we use .first
+    #     BookListItem.where(book_id: params[:book_id], user_id: current_user.id).first.destroy
+    #     redirect_to book_list_items_path
+    # end
 end
