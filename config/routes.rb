53fc1ce82fe63d3adfb7bc1c341c6c3527c18acc
Tony Huynh <t.hnh@tuta.io>
2019-06-25 10:57:02 +1000

    edited syntax in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "books#index"
   get "/books", to: "books#index", as: "books"
   get "/books/new", to: "books#new", as: "new_book"
-  post "books", to: "books#create"
+  post "/books", to: "books#create"
   get "/books/add", to: "books#add", as: "add_book_to_list"
   get "/books/:id", to: "books#show", as: "book"
   get "/authors", to: "authors#index", as: "authors"
