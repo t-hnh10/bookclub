da986c09b6da8e827efbc89d1ee8312acab5265a
Tony Huynh <t.hnh@tuta.io>
2019-07-02 12:00:19 +1000

    added follow list on book list

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,11 @@
 class RelationshipsController < ApplicationController
   def create
+
+    if current_user = nil
+      flash[:alert] = "You must be logged in to do that"
+      return redirect_to new_user_session_path
+    end
+      
     user = User.find(params[:user_id])
     current_user.following.push(user)
 
